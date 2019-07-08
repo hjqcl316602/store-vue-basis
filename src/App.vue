6bdda9d11578097774585c3c9e085a489ac32041
hjqcl316602 <415877608@qq.com>
2019-07-08 16:49:50 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <div class="main"></div>
+    <span class="vc-text--clamp">
+      {{ $store.state.basis.name }}
+    </span>
+    <button @click="change">change</button>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  mounted() {
+    console.log(this.$store);
+  },
+  methods: {
+    change() {
+      this.$store.commit("change/name", "huang");
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+<style lang="less">
+//less 文件不能像css在main.js中直接引入，需要在次处引入即可
+@import "./assets/app.less";
+</style>
+
+
